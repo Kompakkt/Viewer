71b0a867d82217645c5ca70b5a1dbd1bf56705e6
Jan G. Wieners <jan@jan-wieners.de>
2018-02-08 18:08:24 +0100

    Use BabylonJS.

--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -1,11 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Input, ViewChild, HostListener } from '@angular/core';
 
-import * as THREE from 'three';
-
-import "./js/EnableThreeExamples";
-
-import "three/examples/js/controls/OrbitControls";
-import "three/examples/js/loaders/ColladaLoader";
+import * as BABYLON from 'babylonjs';
 
 @Component({
   selector: 'scene',
@@ -14,75 +9,53 @@ import "three/examples/js/loaders/ColladaLoader";
 })
 export class SceneComponent implements AfterViewInit {
 
-  private renderer: THREE.WebGLRenderer;
-  private camera: THREE.PerspectiveCamera;
-  public scene: THREE.Scene;
-
-  public fieldOfView: number = 20;
-  public nearClippingPane: number = 1;
-  public farClippingPane: number = 1100;
-
-  public controls: THREE.OrbitControls;
-
   @ViewChild('canvas')
   private canvasRef: ElementRef;
 
+  private canvas;
+  private engine;
+  private scene;
+
   constructor() {
-    this.render = this.render.bind(this);
-    this.onModelLoadingCompleted = this.onModelLoadingCompleted.bind(this);
   }
 
-  private get canvas(): HTMLCanvasElement {
+  private getCanvas(): HTMLCanvasElement {
     return this.canvasRef.nativeElement;
   }
 
   private createScene() {
-    this.scene = new THREE.Scene();
-    this.scene.add(new THREE.AxisHelper(200));
-    let loader = new THREE.ColladaLoader();
 
-    const mesh = new THREE.Mesh(
-      new THREE.BoxGeometry(500, 500, 500),
-      new THREE.MeshPhongMaterial({
-        color: 0x156289,
-        emissive: 0x072534,
-        side: THREE.DoubleSide,
-        shading: THREE.FlatShading
-      })
-    );
-    this.scene.add(mesh);
+    this.canvas = this.getCanvas();
 
-    loader.load('assets/models/example.dae', this.onModelLoadingCompleted);
-  }
+    // load the 3D engine
+    this.engine = new BABYLON.Engine(this.canvas, true);
 
-  private onModelLoadingCompleted(collada) {
-    let modelScene = collada.scene;
-    this.scene.add(modelScene);
-    this.render();
+    // create a basic BJS Scene object
+    this.scene = new BABYLON.Scene(this.engine);
+    // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
+    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), this.scene);
+    // target the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
+    // attach the camera to the canvas
+    camera.attachControl(this.canvas, false);
+    // create a basic light, aiming 0,1,0 - meaning, to the sky
+    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), this.scene);
+    // create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
+    var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, this.scene);
+    // move the sphere upward 1/2 of its height
+    sphere.position.y = 1;
+    // create a built-in "ground" shape; its constructor takes the same 6 params : name, width, height, subdivision, scene, updatable
+    var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, this.scene);
   }
 
   private createLight() {
-    let light = new THREE.PointLight(0xffffff, 1, 1000);
-    light.position.set(0, 0, 100);
-    this.scene.add(light);
   }
 
   private createCamera() {
-    let aspectRatio = this.getAspectRatio();
-    this.camera = new THREE.PerspectiveCamera(
-      this.fieldOfView,
-      aspectRatio,
-      this.nearClippingPane,
-      this.farClippingPane
-    );
-
-    // Set position and look at
-    this.camera.position.x = 10;
-    this.camera.position.y = 10;
-    this.camera.position.z = 100;
   }
 
   private getAspectRatio(): number {
+
     let height = this.canvas.clientHeight;
     if (height === 0) {
       return 0;
@@ -91,87 +64,21 @@ export class SceneComponent implements AfterViewInit {
   }
 
   private startRendering() {
-    this.renderer = new THREE.WebGLRenderer({
-      canvas: this.canvas,
-      antialias: true
-    });
-    this.renderer.setPixelRatio(devicePixelRatio);
-    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
-
-    this.renderer.shadowMap.enabled = true;
-    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    this.renderer.setClearColor(0xffffff, 1);
-    this.renderer.autoClear = true;
-
-    let component: SceneComponent = this;
-
-    (function render() {
-      //requestAnimationFrame(render);
-      component.render();
-    }());
   }
-
-  public render() {
-    this.renderer.render(this.scene, this.camera);
-  }
-
-  public addControls() {
-    this.controls = new THREE.OrbitControls(this.camera);
-    this.controls.rotateSpeed = 1.0;
-    this.controls.zoomSpeed = 1.2;
-    this.controls.addEventListener('change', this.render);
-
-  }
-
-  /* EVENTS */
 
   public onMouseDown(event: MouseEvent) {
-    console.log("onMouseDown");
-    event.preventDefault();
-
-    // Example of mesh selection/pick:
-    var raycaster = new THREE.Raycaster();
-    var mouse = new THREE.Vector2();
-    mouse.x = (event.clientX / this.renderer.domElement.clientWidth) * 2 - 1;
-    mouse.y = - (event.clientY / this.renderer.domElement.clientHeight) * 2 + 1;
-    raycaster.setFromCamera(mouse, this.camera);
-
-    var obj: THREE.Object3D[] = [];
-    this.findAllObjects(obj, this.scene);
-    var intersects = raycaster.intersectObjects(obj);
-    console.log("Scene has " + obj.length + " objects");
-    console.log(intersects.length + " intersected objects found")
-    intersects.forEach((i) => {
-      console.log(i.object); // do what you want to do with object
-    });
-
-  }
-
-  private findAllObjects(pred: THREE.Object3D[], parent: THREE.Object3D) {
-    // NOTE: Better to keep separate array of selected objects
-    if (parent.children.length > 0) {
-      parent.children.forEach((i) => {
-        pred.push(i);
-        this.findAllObjects(pred, i);
-      });
-    }
   }
 
   public onMouseUp(event: MouseEvent) {
-    console.log("onMouseUp");
   }
-
 
   @HostListener('window:resize', ['$event'])
   public onResize(event: Event) {
+
+    this.engine.resize();
     this.canvas.style.width = "100%";
     this.canvas.style.height = "100%";
     console.log("onResize: " + this.canvas.clientWidth + ", " + this.canvas.clientHeight);
-
-    this.camera.aspect = this.getAspectRatio();
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
-    this.render();
   }
 
   @HostListener('document:keypress', ['$event'])
@@ -179,13 +86,19 @@ export class SceneComponent implements AfterViewInit {
     console.log("onKeyPress: " + event.key);
   }
 
-  /* LIFECYCLE */
   ngAfterViewInit() {
+
     this.createScene();
+
+    let scene = this.scene;
+
+    // run the render loop
+    this.engine.runRenderLoop(function(){
+      scene.render();
+    });
     this.createLight();
     this.createCamera();
     this.startRendering();
-    this.addControls();
   }
 
 }
