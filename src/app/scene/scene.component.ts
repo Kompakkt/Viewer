7d1fd2f52efc5f8ede03657131afd19c5eea5433
Jan G. Wieners <jan@jan-wieners.de>
2018-02-10 15:22:19 +0100

    Tidy up scene component.

--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -32,35 +32,33 @@ export class SceneComponent implements AfterViewInit {
 
     // create a basic BJS Scene object
     this.scene = new BABYLON.Scene(this.engine);
+
+    this.createCamera();
+
+    this.createLight();
+
+    // create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
+    let sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, this.scene);
+    // move the sphere upward 1/2 of its height
+    sphere.position.y = 1;
+    // create a built-in "ground" shape; its constructor takes the same 6 params : name, width, height, subdivision, scene, updatable
+    let ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, this.scene);
+  }
+
+  private createLight() {
+
+    // create a basic light, aiming 0,1,0 - meaning, to the sky
+    let light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), this.scene);
+  }
+
+  private createCamera() {
+
     // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), this.scene);
+    let camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), this.scene);
     // target the camera to scene origin
     camera.setTarget(BABYLON.Vector3.Zero());
     // attach the camera to the canvas
     camera.attachControl(this.canvas, false);
-    // create a basic light, aiming 0,1,0 - meaning, to the sky
-    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), this.scene);
-    // create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
-    var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, this.scene);
-    // move the sphere upward 1/2 of its height
-    sphere.position.y = 1;
-    // create a built-in "ground" shape; its constructor takes the same 6 params : name, width, height, subdivision, scene, updatable
-    var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, this.scene);
-  }
-
-  private createLight() {
-  }
-
-  private createCamera() {
-  }
-
-  private getAspectRatio(): number {
-
-    let height = this.canvas.clientHeight;
-    if (height === 0) {
-      return 0;
-    }
-    return this.canvas.clientWidth / this.canvas.clientHeight;
   }
 
   private startRendering() {
