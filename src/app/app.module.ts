35699072118dec4625186ed75956b37051b71772
Jan G. Wieners <jan@jan-wieners.de>
2018-04-25 21:47:47 +0200

    Prepare language service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {SceneComponent} from './components/scene/scene.component';
-import {ImportService} from './services/import/import.service';
 import {MenuComponent} from './menu/menu.component';
 import {AnnotationsComponent} from './components/annotations/annotations.component';
+
+import {ImportService} from './services/import/import.service';
 import {CameraService} from './services/camera/camera.service';
 import {BabylonService} from './services/engine/babylon.service';
 import {SkyboxService} from './services/skybox/skybox.service';
+import {LanguageService} from './services/language/language.service';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,8 @@ import {SkyboxService} from './services/skybox/skybox.service';
     SkyboxService,
     AnnotationsComponent,
     CameraService,
-    BabylonService
+    BabylonService,
+    LanguageService
   ],
   bootstrap: [AppComponent]
 })
