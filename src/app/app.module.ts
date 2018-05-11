fefacd4002148b78a11daa39ee017e67cd96189d
Jan G. Wieners <jan@jan-wieners.de>
2018-05-11 22:36:58 +0200

    Remove annotations component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {AnnotationsComponent} from './components/annotations/annotations.component';
 import {SceneComponent} from './components/scene/scene.component';
 import {MenuComponent} from './components/menu/menu.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -12,7 +11,6 @@ import { EditorComponent } from './components/editor/editor.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AnnotationsComponent,
     SceneComponent,
     MenuComponent,
     EditorComponent
@@ -27,7 +25,7 @@ import { EditorComponent } from './components/editor/editor.component';
     MatMenuModule,
     MatIconModule
   ],
-  providers: [AnnotationsComponent],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
