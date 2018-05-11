d8dca8ff2afc3a7c38d9fd8af49c9fcff06d757b
Jan G. Wieners <jan@jan-wieners.de>
2018-05-11 22:31:39 +0200

    Add editor component and display editor component in sidenav.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,23 @@ import {AnnotationsComponent} from './components/annotations/annotations.compone
 import {SceneComponent} from './components/scene/scene.component';
 import {MenuComponent} from './components/menu/menu.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatTooltipModule, MatIconModule, MatMenuModule, MatSnackBarModule} from '@angular/material';
+import {MatButtonModule, MatTooltipModule, MatIconModule, MatMenuModule, MatSnackBarModule, MatSidenavModule} from '@angular/material';
+import { EditorComponent } from './components/editor/editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AnnotationsComponent,
     SceneComponent,
-    MenuComponent
+    MenuComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatSnackBarModule,
     MatTooltipModule,
+    MatSidenavModule,
     MatButtonModule,
     MatMenuModule,
     MatIconModule
