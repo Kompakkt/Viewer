050d78707e17cde3e2e76da80b8f3e6a87eabee4
Jan G. Wieners <jan@jan-wieners.de>
2018-08-28 16:33:28 +0200

    Add member variable.

--- src/app/components/annotations/annotations.component.ts
+++ src/app/components/annotations/annotations.component.ts
@@ -3,13 +3,14 @@ import {Annotation} from '../../interfaces/annotation/annotation';
 
 import {CameraService} from '../../services/camera/camera.service';
 
-
 @Component({
   selector: 'app-annotations',
   templateUrl: './annotations.component.html',
   styleUrls: ['./annotations.component.css']
 })
 export class AnnotationsComponent implements OnInit {
+
+  private collapsed = false;
 
   annotation: Annotation = {
     counter: 1,
