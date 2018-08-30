971e60fef15c762ee884be0ef2efdcf4ab91d6ed
Jan G. Wieners <jan@jan-wieners.de>
2018-08-30 13:44:07 +0200

    Refactor annotation service.

--- src/app/services/annotation/annotation.service.ts
+++ src/app/services/annotation/annotation.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
-import * as GUI from 'babylonjs-gui';
-import * as BABYLON from 'babylonjs';
 
-import {CameraService} from '../camera/camera.service';
+import * as BABYLON from 'babylonjs';
+import * as GUI from 'babylonjs-gui';
+
 import {BabylonService} from '../babylon/babylon.service';
 
 @Injectable({
@@ -11,13 +11,9 @@ import {BabylonService} from '../babylon/babylon.service';
 export class AnnotationService {
 
   private canvas: HTMLCanvasElement;
-  private scene: BABYLON.Scene;
   private annotationCounter = 0;
 
-  constructor(
-    private cameraService: CameraService,
-    private babylonService: BabylonService,
-  ) {
+  constructor(private babylonService: BabylonService) {
   }
 
   // load existing annotations (get data) -> draw labels
@@ -28,8 +24,9 @@ export class AnnotationService {
 
     if (unit_mesh.source !== null) {
 
-      const pickResult = this.scene.pick(this.scene.pointerX, this.scene.pointerY,
-        null, false, this.scene.activeCamera);
+      const scene = this.babylonService.getScene();
+      const pickResult = scene.pick(scene.pointerX, scene.pointerY,
+        null, false, scene.activeCamera);
 
       if (pickResult.pickedMesh) {
 
@@ -42,18 +39,24 @@ export class AnnotationService {
   }
 
   public pickableModel(name: string, pickable: boolean) {
-    this.scene = this.babylonService.getScene();
-    const mesh = this.scene.getMeshByName(name);
+
+    const scene = this.babylonService.getScene();
+    const mesh = scene.getMeshByName(name);
+
     if (mesh != null) {
-      mesh.actionManager = new BABYLON.ActionManager(this.scene);
+
+      mesh.actionManager = new BABYLON.ActionManager(scene);
+
       if (pickable) {
+
         const that = this;
-        mesh.actionManager.registerAction(
-          new BABYLON.ExecuteCodeAction(
-            BABYLON.ActionManager.OnDoublePickTrigger, function (evt) {
-              that.mousePickModel(evt);
-            }));
+
+        mesh.actionManager.registerAction(new BABYLON.ExecuteCodeAction(
+          BABYLON.ActionManager.OnDoublePickTrigger, function (evt) {
+            that.mousePickModel(evt);
+          }));
       } else {
+
         if (mesh.actionManager.hasPickTriggers) {
           for (let i = mesh.actionManager.actions.length - 1; i >= 0; i--) {
             mesh.actionManager.actions.splice(i, 1);
@@ -63,12 +66,8 @@ export class AnnotationService {
     }
   }
 
-
-  public drawAnnotation() {
-    //this.createAnnotationLabel(pickResultVector, normal, precedence);
-  }
-
   public createNewAnnotation(position: BABYLON.Vector3, normal: BABYLON.Vector3) {
+
     const precedence = this.annotationCounter;
     this.createAnnotationLabel(position, normal, precedence);
   }
@@ -82,16 +81,34 @@ export class AnnotationService {
     this.createGeometryForLabel('planeup', 'labelup', true, 0.5, 1, position, normal);
   }
 
+  private onMarkerClicked() {
+
+    // Kameraposition einnehmen
+    // HTML Textbox anzeigen
+    alert('works now');
+    // that.updateScreenPosition();
+
+    /*
+  *
+  * Dann kann diese auch onclick leuchten
+  * Dafür die 3D engine in Scene so laden:
+  * this.engine = new BABYLON.Engine(this.canvas, true, { stencil: true });
+  * Add the highlight layer.
+  * var hl = new BABYLON.HighlightLayer("hl1", scene);
+  * hl.addMesh(plane, BABYLON.Color3.Green());
+  * hl.removeMesh(plane);
+  */
+  }
+
   private createGeometryForLabel(namePlane: string, nameLabel: string, clickable: boolean,
                                  alpha: number, renderingGroup: number, position: BABYLON.Vector3,
                                  normal: BABYLON.Vector3) {
-    const plane = BABYLON.MeshBuilder.CreatePlane
-    (
-      namePlane + '_' + String(this.annotationCounter),
-      {height: 1, width: 1},
-      this.scene
-    );
+
+    const plane = BABYLON.MeshBuilder.CreatePlane(namePlane + '_' + String(this.annotationCounter),
+      {height: 1, width: 1}, this.babylonService.getScene());
+
     BABYLON.Tags.AddTagsTo(plane, namePlane);
+
     plane.position = new BABYLON.Vector3(position.x, position.y, position.z);
     plane.translate(normal, 1, BABYLON.Space.WORLD);
     plane.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
@@ -108,24 +125,8 @@ export class AnnotationService {
 
     advancedTexturePlane.addControl(label);
 
-    if (clickable === true) {
-      label.onPointerDownObservable.add(function () {
-        // Kameraposition einnehmen
-        // HTML Textbox anzeigen
-        alert('works');
-        //that.updateScreenPosition();
-        /*
-  *
-  * Dann kann diese auch onclick leuchten
-  * Dafür die 3D engine in Scene so laden:
-  * this.engine = new BABYLON.Engine(this.canvas, true, { stencil: true });
-  * Add the highlight layer.
-  * var hl = new BABYLON.HighlightLayer("hl1", scene);
-  * hl.addMesh(plane, BABYLON.Color3.Green());
-  * hl.removeMesh(plane);
-  */
-
-      });
+    if (clickable) {
+      label.onPointerDownObservable.add(this.onMarkerClicked);
     }
 
     const number = new GUI.TextBlock();
@@ -138,26 +139,25 @@ export class AnnotationService {
     label.addControl(number);
 
     plane.material.alpha = alpha;
-    //TODO: click is not working if renderingGroup = 1 and Object is behind another object
+    // TODO: click is not working if renderingGroup = 1 and Object is behind another object
     plane.renderingGroupId = renderingGroup;
   }
 
 
-  // Onklick
+  // Onclick
   // Position einnehmen (erstmal perfekte Sicht auf die Annotation)
   // HTML Box anzeigen (visible)
 
   private updateScreenPosition() {
 
     const annotation = <HTMLElement>document.querySelector('.annotation');
-    const vector = this.scene.getMeshByName('plane_1').getBoundingInfo().boundingBox.centerWorld;
+    const vector = this.babylonService.getScene().getMeshByName('plane_1').getBoundingInfo().boundingBox.centerWorld;
 
     vector.x = Math.round((0.5 + vector.x / 2) * (this.canvas.width / window.devicePixelRatio));
     vector.y = Math.round((0.5 - vector.y / 2) * (this.canvas.height / window.devicePixelRatio));
 
     annotation.style.top = vector.y + 'px';
     annotation.style.left = vector.x + 'px';
-
   }
 
 // Delete Annotation
