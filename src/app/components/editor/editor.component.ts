27335b5498a4ed0bd5c73a1b2d9e5287bb16f31c
ZetOE <zoeschubert@gmx.de>
2018-10-25 11:57:16 +0200

    Starting to implement cdkDropList -> not yet working because of async loading

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -2,6 +2,7 @@ import {Component, HostBinding, NgZone, OnInit} from '@angular/core';
 import {SidenavService} from '../../services/sidenav/sidenav.service';
 import {AnnotationService} from '../../services/annotation/annotation.service';
 import {ActionService} from '../../services/action/action.service';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 
 @Component({
@@ -33,6 +34,10 @@ export class EditorComponent implements OnInit {
 
   getAnnotations(): void {
     this.annotationService.annotations$ = this.annotationService.fetchAnnotations();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    // moveItemInArray(this.annotationService.annotations$, event.previousIndex, event.currentIndex);
   }
 
 }
