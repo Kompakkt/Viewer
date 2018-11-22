5a0d36b7921511086db6c932096c546f2825c1fd
Kai Niebes <kai.niebes@outlook.com>
2018-11-22 19:25:05 +0100

    Change ModelComponent to wait for the result of the loadModel Promise

--- src/app/components/model/model.component.ts
+++ src/app/components/model/model.component.ts
@@ -26,36 +26,17 @@ export class ModelComponent implements OnInit {
   }
 
   public loadModel(): void {
-    const mesh = this.babylonService.getScene().getMeshByName(this.actualModelName);
+    this.babylonService.getScene().meshes.map(model => model.dispose());
 
-    if (mesh != null) {
-      mesh.dispose();
-    }
-
-
-    const dispose = this.babylonService.getScene().getMeshesByTags('actualModel');
-    dispose.forEach(function (value) {
-      console.log('dispose ' + value);
-      value.dispose();
+    this.babylonService.loadModel('https://blacklodge.hki.uni-koeln.de:8065/models/', this.model.processed.low)
+    .then(model => {
+      // Warte auf Antwort von loadModel, da loadModel ein Promise<object> von ImportMeshAync übergibt
+      // model ist hier das neu geladene Model, aus dem wir direkt den Namen nehmen können
+      BABYLON.Tags.AddTagsTo(model, model.meshes[0].name);
+      this.actionService.pickableModel(model.meshes[0].name, true);
+      this.annotationService.initializeAnnotationMode(model.meshes[0].name);
+      this.actualModelName = model.meshes[0].name;
     });
-
-
-    this.babylonService.loadModel('https://blacklodge.hki.uni-koeln.de:8065/models/', this.model.processed.low);
-    const meshes = this.babylonService.getScene().meshes;
-    BABYLON.Tags.AddTagsTo(meshes[meshes.length - 1], 'actualModel');
-    console.log('NEW');
-    console.log('das letzte Model ' + meshes[meshes.length - 1]);
-    //console.log('nachher ' + this.babylonService.getScene().meshes);
-    this.actionService.pickableModel(meshes[meshes.length - 1].name, true);
-    this.annotationService.initializeAnnotationMode(meshes[meshes.length - 1].name);
-    this.actualModelName = meshes[meshes.length - 1].name;
-
-
-    //console.log(this.model.name);
-    //console.log(this.model.processed.low.split('.').slice(0, -1).join('.').substring(this.model.processed.low.indexOf('/')+1));
-    //this.filename = this.model.name;
-
-
   }
 
 
