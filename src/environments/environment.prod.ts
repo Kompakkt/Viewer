7567bc58182b1c9ac7425696e2c28e4c2edf58cb
Kai Niebes <kai.niebes@outlook.com>
2018-11-22 21:05:59 +0100

    Changed BlackLodge to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   express_server_port: 8065,
   // express_server_url: 'https://miskatonic.hki.uni-koeln.de'
   // TODO: Irgendwann den Express Server auf Miskatonic setzen
-  express_server_url: 'http://blacklodge.hki.uni-koeln.de'
+  express_server_url: 'https://blacklodge.hki.uni-koeln.de'
 };
