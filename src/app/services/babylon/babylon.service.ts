d81914259183a14fcecd48aa6d570aded3d2e6de
Bene <bene_mildenberger@web.de>
2018-11-28 12:49:05 +0100

    finally fix screenshot camera

--- src/app/services/babylon/babylon.service.ts
+++ src/app/services/babylon/babylon.service.ts
@@ -111,7 +111,7 @@ export class BabylonService {
   public createPreviewScreenshot(width?: number): Promise<string> {
     return new Promise<string>((resolve, reject) => {
       if (width === undefined) {
-        BABYLON.Tools.CreateScreenshot(this.getEngine(), this.getScene().activeCamera, 360, (screenshot) => {
+        BABYLON.Tools.CreateScreenshot(this.getEngine(), this.getScene().activeCamera, {precision: 1}, (screenshot) => {
           resolve(screenshot);
         });
       } else {
