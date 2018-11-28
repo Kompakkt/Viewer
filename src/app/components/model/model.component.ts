f5d0c40b6b8201a5c2f5a145347768596a230e0c
Kai Niebes <kai.niebes@outlook.com>
2018-11-28 12:24:03 +0100

    Clean up ModelComponent to use the new LoadModelService

--- src/app/components/model/model.component.ts
+++ src/app/components/model/model.component.ts
@@ -7,6 +7,7 @@ import { AnnotationService } from '../../services/annotation/annotation.service'
 import { CameraService } from '../../services/camera/camera.service';
 import { CatalogueService } from '../../services/catalogue/catalogue.service';
 import { LoadingscreenhandlerService } from '../../services/loadingscreenhandler/loadingscreenhandler.service';
+import { LoadModelService } from '../../services/load-model/load-model.service';
 
 
 @Component({
@@ -19,13 +20,8 @@ export class ModelComponent implements OnInit {
 
   private actualModelName: string;
 
-  constructor(private babylonService: BabylonService,
-    private actionService: ActionService,
-    private annotationService: AnnotationService,
-    private cameraService: CameraService,
-    private catalogueService: CatalogueService,
-    private loadingScreenHandler: LoadingscreenhandlerService
-  ) {
+  constructor(private catalogueService: CatalogueService,
+              private loadModelService: LoadModelService) {
   }
 
   ngOnInit() {
@@ -40,30 +36,6 @@ export class ModelComponent implements OnInit {
   }
 
   public loadModel(newModel: Model) {
-    if (!this.loadingScreenHandler.isLoading) {
-      this.babylonService.getScene().meshes.map(model => model.dispose());
-
-      const modelUrl = 'https://blacklodge.hki.uni-koeln.de:8065/models/';
-      const quality = this.catalogueService.Observables.quality.source['_value'];
-
-      this.babylonService.loadModel(modelUrl, newModel.processed[quality]).then(async (model) => {
-        // Warte auf Antwort von loadModel, da loadModel ein Promise<object> von ImportMeshAync übergibt
-        // model ist hier das neu geladene Model, aus dem wir direkt den Namen nehmen können
-
-        // Zentriere auf das neu geladene Model
-        this.cameraService.setActiveCameraTarget(model.meshes[0]._boundingInfo.boundingBox.centerWorld);
-
-        // Füge Tags hinzu und lade Annotationen
-        BABYLON.Tags.AddTagsTo(model.meshes[0], newModel.name);
-        this.actionService.pickableModel(newModel.name, true);
-
-        // TODO: Irgendwie auf Initialisierung des annotationServices warten
-        setTimeout(() => {
-          while (this.loadingScreenHandler.isLoading) { }
-          this.annotationService.initializeAnnotationMode(newModel.name);
-          this.annotationService.loadAnnotations(newModel.name);
-        }, 500);
-      });
-    }
+    this.loadModelService.loadModel(newModel);
   }
 }
