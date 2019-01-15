de19b58d19018f48f73e16d6ed274ae4c4e5d9c7
Kai Niebes <kai.niebes@outlook.com>
2019-01-15 15:43:10 +0100

    Update endpoint to Miskatonic in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: true,
-  // express_server_url: 'http://localhost',
-  express_server_port: 8065,
-  // express_server_url: 'https://miskatonic.hki.uni-koeln.de'
-  // TODO: Irgendwann den Express Server auf Miskatonic setzen
-  express_server_url: 'https://blacklodge.hki.uni-koeln.de'
+  express_server_url: 'https://miskatonic.hki.uni-koeln.de',
+  express_server_port: 8080,
 };
