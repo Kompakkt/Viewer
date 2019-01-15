9b38fce2831bfc5d1aabe7679b0d558640f6ef8b
Kai Niebes <kai.niebes@outlook.com>
2019-01-15 22:44:26 +0100

    Refactor Mongohandler to use Auth Headers and add Auth functions

--- src/app/services/mongohandler/mongohandler.service.ts
+++ src/app/services/mongohandler/mongohandler.service.ts
@@ -9,63 +9,63 @@ import {Observable} from 'rxjs';
 export class MongohandlerService {
 
   private endpoint = `${environment.express_server_url}:${environment.express_server_port}`;
-  private headers: HttpHeaders;
+    private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    }),
+    withCredentials: true
+  };
 
   constructor(private http: HttpClient) {
-
-    this.headers = new HttpHeaders();
-    this.headers.set('Content-Type', 'application/json');
   }
 
+  // Override GET and POST to use HttpOptions which is needed for auth
+  private get(path: string): Observable<any> {
+    return this.http.get(`${this.endpoint}/${path}`, this.httpOptions);
+  }
+
+  private post(path: string, obj: any): Observable<any> {
+    return this.http.post(`${this.endpoint}/${path}`, obj, this.httpOptions);
+  }
+
+
+  // GETs
   public getAllCompilations(): Observable<any> {
-    return this.http.get<any>(this.endpoint + '/api/v1/get/findall/compilation', {headers: this.headers});
+    return this.get(`api/v1/get/findall/compilation`);
   }
 
   public getAllModels(): Observable<any> {
-    return this.http.get<any[]>(this.endpoint + '/api/v1/get/findall/model', {headers: this.headers});
-    // return this.http.get<Model[]>(`${this.endpoint}/api/v1/get/findall/${Collection.Model}`).toPromise();
+    return this.get(`api/v1/get/findall/model`);
   }
 
   public getModel(identifier: string): Observable<any> {
-    return this.http.get<any[]>(this.endpoint + '/api/v1/get/find/model' + identifier, {headers: this.headers});
-    // return this.http.get<Model[]>(`${this.endpoint}/api/v1/get/findall/${Collection.Model}`).toPromise();
+    return this.get(`api/v1/get/find/model/${identifier}`);
   }
   public getCompilation(identifier: string): Observable<any> {
-    return this.http.get<any>(this.endpoint + '/api/v1/get/find/compilation/' + identifier, {headers: this.headers});
+    return this.get(`api/v1/get/find/compilation//${identifier}`);
   }
 
   public getModelMetadata(identifier: string): Observable<any> {
-    return this.http.get<any>(this.endpoint + '/api/v1/get/find/digitalobject/' + identifier, {headers: this.headers});
+    return this.get(`api/v1/get/find/digitalobject/${identifier}`);
   }
 
+  // POSTs
   public updateScreenshot(identifier: string, screenshot: string): Observable<any> {
-    return this.http.post<any>(this.endpoint + '/api/v1/post/screenshot/' + identifier, {data: screenshot});
+    return this.post(`api/v1/post/screenshot/${identifier}`, {data: screenshot});
   }
 
   public updateCameraPos(identifier: string, cameraPerspective: any): Observable<any> {
     console.log('Camera soll gespeichert werden');
-    return this.http.post<any>(this.endpoint + '/api/v1/post/cameraPosition/' + identifier, {data: cameraPerspective});
+    return this.post(`api/v1/post/cameraPosition/${identifier}`, {data: cameraPerspective});
   }
 
-  /* Funktionen aus ObjectsRepository, nur zum Vergleich
-    private findSingleInCollection(collection: Collection, identifier: string): Observable<Person | Institute | Tag> {
-      return this.http.get<Person | Institute>(`${this.endpoint}/api/v1/get/find/${collection}/${identifier}`);
-    }
-
-    public findAllInCollection(collection: Collection): Observable<Person[] | Institute[] | Tag[]> {
-      return this.http.get<Person[] | Institute[]>(`${this.endpoint}/api/v1/get/findall/${collection}`);
-    }
-
-    public submitToDB(SubmitObject: any): Observable<any> {
-      return this.http.post(`${this.endpoint}/api/v1/post/submit`, SubmitObject);
-    }
-
-  public getCompilation(identifier: string): Promise<Compilation> {
-    return this.http.get<Compilation>(`${this.endpoint}/api/v1/get/find/${Collection.Compilation}/${identifier}`).toPromise();
+  // Auth
+  public login(username: string, password: string): Observable<any> {
+    return this.post(`login`, {username: username, password: password});
   }
 
-    public getModelMetadata(identifier: string): Promise<any> {
-    return this.http.get(`${this.endpoint}/api/v1/get/find/${Collection.DigitalObject}/${identifier}`).toPromise();
+  public isAuthorized() {
+    return this.get(`auth`);
   }
-  */
+
 }
