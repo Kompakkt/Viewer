0b46c951612ead2cb525c33c92b06f79867e94fc
ZetOE <zoeschubert@gmx.de>
2019-01-18 00:28:29 +0100

    Login feedback.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,6 +12,7 @@ export class LoginComponent implements OnInit {
 
   public username = '';
   public password = '';
+  public success = false;
 
   constructor(private mongohandlerService: MongohandlerService,
               private catalogueService: CatalogueService,
@@ -28,6 +29,7 @@ export class LoginComponent implements OnInit {
     this.mongohandlerService.login(this.username, this.password).subscribe(result => {
         if (result.status === 'ok') {
           this.catalogueService.bootstrap();
+          this.success = true;
         }
       }, error => {
         this.message.error('Connection to object server refused.');
