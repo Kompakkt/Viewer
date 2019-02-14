4cec71ad8014edb3e86f1eccfc89ecbb9c7cc79f
ZetOE <zoeschubert@gmx.de>
2019-02-14 21:02:13 +0100

    Log Values.

--- src/app/components/modelsettings/modelsettings.component.ts
+++ src/app/components/modelsettings/modelsettings.component.ts
@@ -406,12 +406,16 @@ export class ModelsettingsComponent implements OnInit {
       }
 
 
+      console.log('Actual Values: ', this.activeModel.settings.rotation,
+        this.activeModel.settings.scale, this.isModelOwner, this.isFinished, this.initialSettingsMode, this.isDefault );
       if (this.activeModel.settings.rotation === undefined || this.activeModel.settings.scale === undefined) {
 
         if (this.isModelOwner && !this.isFinished) {
 
           this.modelSettingsService.createVisualSettings();
           this.initialSettingsMode = true;
+          console.log('Actual Values: ', this.activeModel.settings.rotation, this.activeModel.settings.scale,
+            this.isModelOwner, this.isFinished, this.initialSettingsMode, this.isDefault );
 
         } else {
 
