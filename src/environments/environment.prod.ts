e4c6a53a6e84d02e78b569b2db09b58fec5c7fe8
Kai Niebes <kai.niebes@outlook.com>
2019-02-18 15:08:47 +0100

    Change dev branch to use dev server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   express_server_url: 'https://miskatonic.hki.uni-koeln.de',
-  express_server_port: 8080,
+  express_server_port: 1337,
   version: require('../../package.json').version
 };
