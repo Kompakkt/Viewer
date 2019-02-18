cc5db762ae559c0f5b999f1d97878a89ac487d76
Kai Niebes <kai.niebes@outlook.com>
2019-02-18 21:47:36 +0100

    Change prod Port to 8080 (main server) so we dont accidentally overwrite it when merging to master at some point

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   express_server_url: 'https://miskatonic.hki.uni-koeln.de',
-  express_server_port: 1337,
+  express_server_port: 8080,
   version: require('../../package.json').version
 };
