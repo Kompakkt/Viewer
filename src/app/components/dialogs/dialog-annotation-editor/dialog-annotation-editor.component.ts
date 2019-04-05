866771456d1a2d85bc36a1f6d3be9d4609fc8d07
Jan G. Wieners <jan@jan-wieners.de>
2019-04-05 21:08:13 +0200

    Enable users to switch between edit- and view-mode.

--- src/app/components/dialogs/dialog-annotation-editor/dialog-annotation-editor.component.ts
+++ src/app/components/dialogs/dialog-annotation-editor/dialog-annotation-editor.component.ts
@@ -13,8 +13,27 @@ export interface IDialogData {
 })
 export class DialogAnnotationEditorComponent {
 
+  public editMode = false;
+  public labelMode = 'edit';
+  public labelModeText = 'Edit';
+
   constructor(public dialogRef: MatDialogRef<DialogAnnotationEditorComponent>,
               @Inject(MAT_DIALOG_DATA) public data: IDialogData) {
+  }
+
+  public toggleEditViewMode() {
+
+    if (this.editMode) {
+
+      this.editMode = false;
+      this.labelMode = 'edit';
+      this.labelModeText = 'Edit';
+    } else {
+
+      this.editMode = true;
+      this.labelMode = 'remove_red_eye';
+      this.labelModeText = 'View';
+    }
   }
 
   cancel(): void {
