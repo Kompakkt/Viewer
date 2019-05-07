c31c5a72137c009f92e737ed072c20d09dabb526
ZetOE <zoeschubert@gmx.de>
2019-05-07 20:51:49 +0200

    Make mesh click or not clickable.

--- src/app/services/annotation/annotation.service.ts
+++ src/app/services/annotation/annotation.service.ts
@@ -634,6 +634,7 @@ export class AnnotationService {
     this.isAnnotatingAllowed = emitBool;
     this.annotationMode(emitBool);
     this.annnotatingAllowed.emit(emitBool);
+    this.annotationMode(emitBool);
   }
 
   public setCollectionInput(selected: boolean) {
