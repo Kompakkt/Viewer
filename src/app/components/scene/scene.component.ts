dcc4fb60833fc758b9fc0a56338b9730432f87e3
Kai Niebes <kai.niebes@outlook.com>
2019-06-23 18:28:22 +0200

    Show Login Component before loading canvas if not logged in

--- src/app/components/scene/scene.component.ts
+++ src/app/components/scene/scene.component.ts
@@ -1,8 +1,11 @@
 import {AfterViewInit, Component, ElementRef, HostListener, ViewChild, ViewContainerRef} from '@angular/core';
+import {MatDialog, MatDialogConfig} from '@angular/material';
 
 import {AnnotationService} from '../../services/annotation/annotation.service';
 import {BabylonService} from '../../services/babylon/babylon.service';
+import {MongohandlerService} from '../../services/mongohandler/mongohandler.service';
 import {ProcessingService} from '../../services/processing/processing.service';
+import {LoginComponent} from '../dialogs/dialog-login/login.component';
 
 @Component({
   selector: 'app-scene',
@@ -20,12 +23,46 @@ export class SceneComponent implements AfterViewInit {
   constructor(private babylonService: BabylonService,
               private processingService: ProcessingService,
               public annotationService: AnnotationService,
-              private viewContainerRef: ViewContainerRef) {
+              private viewContainerRef: ViewContainerRef,
+              private mongo: MongohandlerService,
+              private dialog: MatDialog) {
+  }
+
+  private loginAttempt() {
+    this.mongo.isAuthorized()
+      .then(result => {
+        if (result.status === 'ok') {
+          this.setupCanvas();
+        } else {
+          // Show Login Screen before loading Babylon
+          this.openLoginDialog();
+        }
+      })
+      .catch(e => console.error(e));
+  }
+
+  private openLoginDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    this.dialog
+      .open(LoginComponent, dialogConfig)
+      .afterClosed()
+      .toPromise()
+      .then(() => this.loginAttempt())
+      .catch(e => {
+        console.error(e);
+        this.loginAttempt();
+      });
+  }
+
+  private setupCanvas() {
+    this.babylonService.attachCanvas(this.viewContainerRef);
+    this.babylonService.resize();
+    this.processingService.bootstrap();
   }
 
   ngAfterViewInit() {
-    this.babylonService.attachCanvas(this.viewContainerRef);
-    this.processingService.bootstrap();
-    this.babylonService.resize();
+    this.loginAttempt();
   }
 }
