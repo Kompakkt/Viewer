c7791f65d22f986a129557ba282d7a9507ba2f78
Kai Niebes <kai.niebes@outlook.com>
2019-08-02 23:50:28 +0200

    Add husky + prettier + precise-commits to auto format on precommit

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  parser: 'typescript',
+  requirePragma: false,
+  insertPragma: false,
+  endOfLine: 'lf',
+  printWidth: 80,
+};
