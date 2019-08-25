08361f3615d7ed9c9493b0399cd5b2c19b328766
Kai Niebes <kai.niebes@outlook.com>
2019-08-25 22:30:33 +0200

    Update prod URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  express_server_url: 'https://miskatonic.hki.uni-koeln.de',
+  express_server_url: 'https://kompakkt.uni-koeln.de',
   express_server_port: 8080,
   version: require('../../package.json').version,
-  repository:
-    'https://blacklodge.hki.uni-koeln.de/builds/ObjectsRepository/live/#',
+  repository: 'https://kompakkt.uni-koeln.de/',
 };
