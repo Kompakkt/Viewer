5d831b2079b5b281409c77f5fe2c4d3bdbac9f36
ZetOE <zoeschubert@gmx.de>
2019-08-26 18:16:53 +0200

    Revert "Load annotation mode correctly"
    This reverts commit aaf02da307aa10217c0c782e7af2d7628712b870.

--- src/app/services/processing/processing.service.ts
+++ src/app/services/processing/processing.service.ts
@@ -363,9 +363,8 @@ export class ProcessingService {
               this.lightMode.emit(false);
               this.overlayService.activateAnnotationsTab();
               // tslint:disable-next-line:max-line-length
-            } else {
               this.message.error(
-                  'You are not logged in and this would be necessary to annotate this object.' +
+                'You are not logged in and this would be necessary to annotate this object.' +
                   'Please reload the page if you want to log in.',
               );
               this.isLightMode = true;
@@ -451,7 +450,7 @@ export class ProcessingService {
       }
     }
 
-    if (loadingCase === 'collection' && !entityParam) {
+    if (loadingCase === 'collection') {
       this.fetchAndLoad(
         undefined,
         compParam ? compParam : undefined,
