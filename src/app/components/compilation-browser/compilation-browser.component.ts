1fbafd69e9dd9ff577e69e36c6b868cddcf64295
ZetOE <zoeschubert@gmx.de>
2019-09-30 17:27:59 +0200

    do not subscribe in compilation browser

--- src/app/components/compilation-browser/compilation-browser.component.ts
+++ src/app/components/compilation-browser/compilation-browser.component.ts
@@ -1,9 +1,5 @@
-import {
-  Component,
-  OnInit,
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import {ICompilation, IEntity} from '../../interfaces/interfaces';
 import { ProcessingService } from '../../services/processing/processing.service';
 
 @Component({
@@ -12,21 +8,7 @@ import { ProcessingService } from '../../services/processing/processing.service'
   styleUrls: ['./compilation-browser.component.scss'],
 })
 export class CompilationBrowserComponent implements OnInit {
+  constructor(public processingService: ProcessingService) {}
 
-  public compilation: ICompilation | undefined;
-  public entity: IEntity | undefined;
-
-  constructor(
-    public processingService: ProcessingService,
-  ) {}
-
-  ngOnInit() {
-    this.processingService.Observables.actualCompilation.subscribe(compilation => {
-      this.compilation = compilation;
-    });
-
-    this.processingService.Observables.actualEntity.subscribe(entity => {
-      this.entity = entity;
-    });
-  }
+  ngOnInit() {}
 }
