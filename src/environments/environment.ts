4fff7d0706087d576c624fece5b2e7095f11cc51
janwieners <jan@jan-wieners.de>
2019-10-28 12:39:14 +0100

    Correct port number.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,10 +7,9 @@
 export const environment = {
   production: false,
   express_server_url: 'https://miskatonic.hki.uni-koeln.de',
-  express_server_port: 1337,
+  express_server_port: 8080,
   version: require('../../package.json').version,
-  repository:
-    'https://blacklodge.hki.uni-koeln.de/builds/ObjectsRepository/dev/#',
+  repository: 'https://blacklodge.hki.uni-koeln.de/builds/ObjectsRepository/dev/#',
 };
 
 // Local:
