78eb95d67c9dbf456cfbcc0dbdb6811d11c69964
Kai Niebes <kai.niebes@outlook.com>
2020-02-18 22:32:49 +0100

    Environment: Update Repo URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   express_server_url: 'https://kompakkt.uni-koeln.de',
   express_server_port: 8080,
   version: require('../../package.json').version,
-  repository: 'https://kompakkt.uni-koeln.de/',
+  repository: 'https://kompakkt.de/',
 };
