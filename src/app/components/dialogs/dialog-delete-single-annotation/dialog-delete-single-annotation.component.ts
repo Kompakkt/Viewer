1cd03a32d6cf461ae62d4745e022afa7646bb020
Jan Wieners <mail@jan-wieners.de>
2021-12-03 20:49:12 +0100

    Remove unused code.

--- src/app/components/dialogs/dialog-delete-single-annotation/dialog-delete-single-annotation.component.ts
+++ src/app/components/dialogs/dialog-delete-single-annotation/dialog-delete-single-annotation.component.ts
@@ -1,40 +1,18 @@
 import { Component } from '@angular/core';
 
-/*import {MessageService} from '../../../services/message/message.service';
-import {ProcessingService} from '../../../services/processing/processing.service';
-import {UserdataService} from '../../../services/userdata/userdata.service';*/
-
 @Component({
-  selector: 'app-dialog-delete-single-annotation',
-  templateUrl: './dialog-delete-single-annotation.component.html',
-  styleUrls: ['./dialog-delete-single-annotation.component.scss'],
-})
+               selector: 'app-dialog-delete-single-annotation',
+               templateUrl: './dialog-delete-single-annotation.component.html',
+               styleUrls: ['./dialog-delete-single-annotation.component.scss'],
+           })
 export class DialogDeleteSingleAnnotationComponent {
-  public username = '';
-  public password = '';
-  public success = false;
+    public username = '';
+    public password = '';
 
-  /*constructor(private processing: ProcessingService,
-              private message: MessageService,
-              private userdata: UserdataService) { }*/
+    public login() {
+    }
 
-  public login() {
-    /*
-    if (this.userdata.cachedUser.username && this.userdata.cachedUser.password) {
-      if (this.userdata.cachedUser.username === this.username &&
-        this.userdata.cachedUser.password === this.password) {
-        this.success = true;
-      } else {
-        this.success = false;
-        this.message.error('Sorry, this was not correct.');
-      }
-    } else {
-      this.success = false;
-      this.message.error('Sorry, you are not logged in.');
-    }*/
-  }
-
-  public withoutlogin() {
-    console.log('username:' + this.username);
-  }
+    public withoutlogin() {
+        console.log(`username: ${this.username}`);
+    }
 }
