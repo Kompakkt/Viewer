caf0b57ee0091ea1c9ee0b9d3e2cf08ae1c1ae01
Kai Niebes <kai.niebes@outlook.com>
2023-03-22 17:15:28 +0100

    Adjust minimum and maximum limits of cameras

--- src/app/services/babylon/camera-handler.ts
+++ src/app/services/babylon/camera-handler.ts
@@ -33,34 +33,25 @@ export const createDefaultCamera = (scene: Scene, canvas: HTMLCanvasElement) => 
     (scene.activeCamera as ArcRotateCamera).dispose();
     scene.activeCamera = null;
   }
-  // Camera
-  const worldExtends = scene.getWorldExtends();
-  const worldSize = worldExtends.max.subtract(worldExtends.min);
-  const worldCenter = worldExtends.min.add(worldSize.scale(0.5));
 
-  let camera: ArcRotateCamera;
-  let radius = worldSize.length() * 1.5;
-  // empty scene scenario!
-  if (!isFinite(radius)) {
-    radius = 1;
-    worldCenter.copyFromFloats(0, 0, 0);
-  }
-
-  // Parameters: alpha, beta, radius, target position, scene
-  const arcRotateCamera = new ArcRotateCamera(
+  const defaultRadius =
+    Math.max(
+      ...Object.values(scene.getWorldExtends())
+        .flatMap(vector => vector.asArray())
+        .map(v => Math.abs(v)),
+    ) * 1.5;
+  const camera = new ArcRotateCamera(
     'arcRotateCamera',
     -(Math.PI / 2),
     Math.PI / 2,
-    radius,
-    worldCenter,
+    defaultRadius,
+    Vector3.Zero(),
     scene,
   );
-  arcRotateCamera.lowerRadiusLimit = radius * 0.01;
-  camera = arcRotateCamera;
-
-  camera.minZ = radius * 0.01;
-  camera.maxZ = radius * 1000;
-  camera.speed = radius * 0.2;
+  camera.lowerRadiusLimit = 0.1;
+  camera.upperRadiusLimit = 45_000;
+  camera.minZ = 0.05;
+  camera.maxZ = 50_000;
   scene.activeCamera = camera;
 
   scene.activeCamera.attachControl(canvas);
@@ -77,6 +68,8 @@ export const createDefaultCamera = (scene: Scene, canvas: HTMLCanvasElement) => 
 
 export const createUniversalCamera = (scene: Scene) => {
   const camera = new UniversalCamera('UniversalCamera', Vector3.Zero(), scene);
+  camera.minZ = 0.05;
+  camera.maxZ = 50_000;
   return camera;
 };
 
