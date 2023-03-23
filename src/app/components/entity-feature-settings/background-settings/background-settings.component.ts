2853999a276acdc33d950af760d7d532d0e9fbff
Kai Niebes <kai.niebes@outlook.com>
2023-03-23 22:16:49 +0100

    Choose where BackgroundSettingsComponent is expanded

--- src/app/components/entity-feature-settings/background-settings/background-settings.component.ts
+++ src/app/components/entity-feature-settings/background-settings/background-settings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RGBA } from 'ngx-color';
 import { EntitySettingsService } from 'src/app/services/entitysettings/entitysettings.service';
 import { ProcessingService } from 'src/app/services/processing/processing.service';
@@ -9,6 +9,8 @@ import { ProcessingService } from 'src/app/services/processing/processing.servic
   styleUrls: ['./background-settings.component.scss'],
 })
 export class BackgroundSettingsComponent {
+  @Input('expanded') public expanded = false;
+
   constructor(public processing: ProcessingService, public entitySettings: EntitySettingsService) {}
 
   public changeColor(color: RGBA) {
