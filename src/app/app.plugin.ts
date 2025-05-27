9ff7537ba64149432b53736f196a10ee19201fc8
Kai Niebes <kai.niebes@outlook.com>
2025-05-27 04:37:41 +0200

    Initiate sk-main branch with Semantic Kompakkt plugins

--- src/app/app.plugin.ts
+++ src/app/app.plugin.ts
@@ -1,14 +1,11 @@
 import { provideExtender } from '@kompakkt/extender';
-/*import { HelloWorldPlugin } from '@kompakkt/plugin-hello-world';
-import { TranslatePlugin } from '@kompakkt/plugin-i18n';
-import { SemanticKompakktMetadataPlugin } from '@kompakkt/plugin-semantic-kompakkt-metadata';*/
+import { HelloWorldPlugin } from '@kompakkt/plugins/hello-world';
+import { SemanticKompakktMetadataPlugin } from '@kompakkt/plugins/semantic-kompakkt-metadata';
 import { BackendService } from './services/backend/backend.service';
 import { TranslatePipe } from './pipes/translate.pipe';
 
 export const pluginProviders = provideExtender({
-  plugins: [
-    /*new HelloWorldPlugin(), new TranslatePlugin(), new SemanticKompakktMetadataPlugin()*/
-  ],
+  plugins: [new HelloWorldPlugin(), new SemanticKompakktMetadataPlugin()],
   componentSet: 'viewerComponents',
   services: {
     backendService: BackendService,
